5b70edf71dfafce98bd1eadad6d6c5ea73c1f0a2
tusharsingh1702 <tusharsingh1702@gmail.com>
2024-03-12 23:43:32 +0530

    Update combos.cpp

--- Recursion/Combos:Hard/combos.cpp
+++ Recursion/Combos:Hard/combos.cpp
@@ -30,3 +30,44 @@
 //         getAns(0,board,ans,leftRow,lowerDiagonal,upperDiagonal,n);
 //         return ans;
 //     }
+
+// Sudoku Solver
+//     void solveSudoku(vector<vector<char>>& board) {
+//          solve(board);
+//     }
+//     bool solve(vector<vector<char>>&board){
+//         for(int i=0;i<9;i++){
+//             for(int j=0;j<9;j++){
+//                 if(board[i][j]=='.'){
+//                     for(char c='1';c<='9';c++){
+//                         if(isValid(board,c,i,j)){
+//                             board[i][j]=c;
+//                             if(solve(board)){
+//                                 return true;
+//                             }
+//                             else{
+//                                 board[i][j]='.';
+//                             }
+
+//                         }
+//                     }
+//                         return false;
+//                 }
+//             }
+//         }
+//                 return true;
+//     }
+//     bool isValid(vector<vector<char>>&board,char c,int row,int col){
+//          for(int i=0;i<9;i++){
+//             if(board[i][col]==c){
+//                 return false;
+//             }
+//             if(board[row][i]==c){
+//                 return false;
+//             }
+//             if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)]==c){
+//                 return false;
+//             }
+//          }
+//          return true;
+//     }
