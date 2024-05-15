0644d650d8469f481fb4f07aec7b1cc3da5e8e01
tusharsingh1702 <tusharsingh1702@gmail.com>
2024-05-15 18:15:28 +0530

    Update 1d.cpp

--- Binary Search/1d.cpp
+++ Binary Search/1d.cpp
@@ -252,5 +252,56 @@
 //         }
 //         return index;
 // 	}
+// };
 
+//Single element in a sorted array
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1){
+//             return nums[0];
+//         }
+//         if(nums[0]!=nums[1])return nums[0];
+//         if(nums[n-1]!=nums[n-2])return nums[n-1];
+//         int low=1,high=n-2;
+//         while(low<=high){
+//             int mid=low+(high-low)/2;
+//             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+//                 return nums[mid];
+//             }
+//             if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
+//                 low=mid+1;
+//             }
+//             else{
+//                 high=mid-1;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+//Find peak element
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1)return 0;
+//         if(nums[0]>nums[1])return 0;
+//         if(nums[n-1]>nums[n-2])return n-1;
+//         int low=1,high=n-2;
+//         while(low<=high){
+//             int mid=low+(high-low)/2;
+//             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+//                 return mid;
+//             }
+//             if(nums[mid]>nums[mid-1]){
+//                 low=mid+1;
+//             }
+//             else{
+//                 high=mid-1;
+//             }
+//         }
+//         return -1;
+//     }
 // };
