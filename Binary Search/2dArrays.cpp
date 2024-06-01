b63e02303762506cd32d92b01041e6961526b9e0
tusharsingh1702 <tusharsingh1702@gmail.com>
2024-06-01 21:12:36 +0530

    Update 2dArrays.cpp

--- Binary Search/2dArrays.cpp
+++ Binary Search/2dArrays.cpp
@@ -92,42 +92,130 @@
 // };
 
 //Matrix Median
-class Solution{   
-public:
-    void findHighLow(vector<vector<int>>&matrix,int&low,int&high){
-        int m=matrix.size(),n=matrix[0].size();
-        int maxi=INT_MIN,mini=INT_MAX;
-        for(int i=0;i<m;i++){
-            mini=min(mini,matrix[i][0]);
-        }
-        for(int i=0;i<m;i++){
-            maxi=max(maxi,matrix[i][n-1]);
-        }
-        low=mini;high=maxi;
-    }
-    int findCount(vector<vector<int>>&matrix,int mid,int R,int C){
-        int count=0;
-        for(int i=0;i<R;i++){
-            int up=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
-            count+=up;
-        }
-        return count;
-    }
-    int median(vector<vector<int>> &matrix, int R, int C){
-        int low,high;
-        findHighLow(matrix,low,high);
-        // cout<<low<<endl;
-        // cout<<high<<endl;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int count=findCount(matrix,mid,R,C);
-            if(count<=(R*C)/2){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return low;
-    }
-};
+// class Solution{   
+// public:
+//     void findHighLow(vector<vector<int>>&matrix,int&low,int&high){
+//         int m=matrix.size(),n=matrix[0].size();
+//         int maxi=INT_MIN,mini=INT_MAX;
+//         for(int i=0;i<m;i++){
+//             mini=min(mini,matrix[i][0]);
+//         }
+//         for(int i=0;i<m;i++){
+//             maxi=max(maxi,matrix[i][n-1]);
+//         }
+//         low=mini;high=maxi;
+//     }
+//     int findCount(vector<vector<int>>&matrix,int mid,int R,int C){
+//         int count=0;
+//         for(int i=0;i<R;i++){
+//             int up=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+//             count+=up;
+//         }
+//         return count;
+//     }
+//     int median(vector<vector<int>> &matrix, int R, int C){
+//         int low,high;
+//         findHighLow(matrix,low,high);
+//         // cout<<low<<endl;
+//         // cout<<high<<endl;
+//         while(low<=high){
+//             int mid=low+(high-low)/2;
+//             int count=findCount(matrix,mid,R,C);
+//             if(count<=(R*C)/2){
+//                 low=mid+1;
+//             }
+//             else{
+//                 high=mid-1;
+//             }
+//         }
+//         return low;
+//     }
+// };
+
+//Painter's partition
+// Similar to split array largest sum
+
+//Minimise maximum distance to to gas station
+
+//Median of 2 sorted arrays
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         int n1=nums1.size(),n2=nums2.size();
+//         if(n1>n2){
+//             return findMedianSortedArrays(nums2,nums1);
+//         }
+//         int left=(n1+n2+1)/2,low=0,high=n1,n=n1+n2;
+//         while(low<=high){
+//             int mid1=low+(high-low)/2;
+//             int mid2=left-mid1,l1=INT_MIN,l2=INT_MIN,r1=INT_MAX,r2=INT_MAX;
+//             if(mid1<n1){
+//                 r1=nums1[mid1];
+//             }
+//             if(mid2<n2){
+//                 r2=nums2[mid2];
+//             }
+//            if(mid1-1>=0){
+//                 l1=nums1[mid1-1];
+//             } 
+//             if(mid2-1>=0){
+//                 l2=nums2[mid2-1];
+//             }
+//             if(l1<=r2 && l2<=r1){
+//                 if(n%2==0){
+//                     double ans=(double)(max(l1,l2)+min(r1,r2))/2;
+//                     return ans;
+//                 }
+//                 else{
+//                     return (double)max(l1,l2);
+//                 }
+//             }
+//             else if(l1>r2){
+//                 high=mid1-1;
+//             }
+//             else{
+//                 low=mid1+1;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+//kth element of two sorted arrays
+//  class Solution{
+//     public:
+//     int kthElement(int arr1[], int arr2[], int n, int m, int k)
+//     {
+//         if(n>m){
+//             return kthElement(arr2,arr1,m,n,k);
+//         }
+//         int left=k,low=max(0,k-m),high=min(k,n);
+//         while(low<=high){
+//             int mid1=low+(high-low)/2;
+//             int mid2=left-mid1,l1=INT_MIN,l2=INT_MIN,r1=INT_MAX,r2=INT_MAX;
+//             if(mid1-1>=0){
+//                 l1=arr1[mid1-1];
+//             }
+//             if(mid2-1>=0){
+//                 l2=arr2[mid2-1];
+//             }
+//             if(mid1<n){
+//                 r1=arr1[mid1];
+//             }
+//              if(mid2<m){
+//                 r2=arr2[mid2];
+//             }
+//             if(l1<=r2 && l2<=r1){
+//                 return max(l1,l2);
+//             }
+//             else if(l1>l2){
+//                 high=mid1-1;
+//             }
+//             else{
+//                 low=mid1+1;
+//             }
+            
+//         }
+//         return -1;
+//     }
+// };
