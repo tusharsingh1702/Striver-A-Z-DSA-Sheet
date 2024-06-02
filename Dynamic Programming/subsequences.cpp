cbb4a66a1fbf6c74ba9905f0381f3ada7d3895fe
tusharsingh1702 <tusharsingh1702@gmail.com>
2024-06-03 00:16:21 +0530

    Update subsequences.cpp

--- Dynamic Programming/subsequences.cpp
+++ Dynamic Programming/subsequences.cpp
@@ -153,3 +153,379 @@
 	  
 // };
 
+//Count subsets with sum K(V. V. Imp. Question)
+//Memoization Solution
+// class Solution{
+//     const int MOD=1e9+7;
+//     vector<vector<int>>dp;
+// 	public:
+// 	int getAns(int*arr,int index,int target){
+// 	    if(index==0){
+// 	        if(target==0 && arr[index]==0){
+// 	            return 2;
+// 	        }
+// 	       else if(target==0 || arr[index]==target){
+// 	            return 1;
+// 	        }
+// 	        else{
+// 	       return 0;
+	            
+// 	        }
+// 	    }
+// 	    if(dp[index][target]!=-1){
+// 	        return dp[index][target];
+// 	    }
+// 	    //not Taken
+// 	    int notTaken=getAns(arr,index-1,target);
+// 	    //taken
+// 	    int taken=0;
+// 	    if(arr[index]<=target){
+// 	    taken=getAns(arr,index-1,target-arr[index]);
+// 	    }
+// 	    return dp[index][target]= (notTaken+taken)%MOD;
+// 	}
+// 	int perfectSum(int arr[], int n, int sum)
+// 	{
+// 	    dp=vector<vector<int>>(n,vector<int>(sum+1,-1));
+//         return getAns(arr,n-1,sum);
+// 	}	  
+// };
+
+//Tabulation Solution
+	// int perfectSum(int arr[], int n, int sum)
+	// {
+	//     dp=vector<vector<int>>(n,vector<int>(sum+1,0));
+      //   if(arr[0]==0){
+      //       dp[0][0]=2;
+      //   }
+      //   else{
+      //       dp[0][0]=1;
+      //   }
+      //   if(arr[0]!=0 && arr[0]<=sum){
+      //  dp[0][arr[0]]=1;
+      //   }
+      //   for(int index=1;index<n;index++){
+      //       for(int target=0;target<=sum;target++){
+	//             //not Taken
+	//             int notTaken=dp[index-1][target];
+	//             //taken
+	//             int taken=0;
+	//             if(arr[index]<=target){
+	//             taken=dp[index-1][target-arr[index]];
+	//             }
+	//             dp[index][target]=(taken+notTaken)%MOD;
+      //       }
+      //   }
+      //   return dp[n-1][sum];
+	// }
+
+//Space Optimization
+// 	int perfectSum(int arr[], int n, int sum)
+// 	{
+// 	    vector<int>prev(sum+1,0),curr(sum+1,0);
+//         if(arr[0]==0){
+//             prev[0]=2;
+//         }
+//         else{
+//             prev[0]=1;
+//         }
+//         if(arr[0]!=0 && arr[0]<=sum){
+//        prev[arr[0]]=1;
+//         }
+//         for(int index=1;index<n;index++){
+//             for(int target=0;target<=sum;target++){
+// 	            //not Taken
+// 	            int notTaken=prev[target];
+// 	            //taken
+// 	            int taken=0;
+// 	            if(arr[index]<=target){
+// 	            taken=prev[target-arr[index]];
+// 	            }
+// 	            curr[target]=(taken+notTaken)%MOD;
+//             }
+//             prev=curr;
+//         }
+//         return prev[sum];
+// 	}	  
+// };
+
+//Partitions with given difference
+//Memoization Solution
+// class Solution {
+//   public:
+//   const int MOD=1e9+7;
+//    vector<vector<int>>dp;
+//     int getAns(vector<int>&arr,int index,int target){
+//         if(index==0){
+//             if(target==0 && arr[0]==0){
+//                 return 2;
+//             }
+//            else if(target==0 || arr[index]==target){
+//                 return 1;
+//             }
+//             else{
+//                 return 0;
+//             }
+            
+//         }
+//         if(dp[index][target]!=-1){
+//             return dp[index][target];
+//         }
+//         //notTaken
+//         int notTaken=getAns(arr,index-1,target);
+//         int taken=0;
+//         if(arr[index]<=target){
+//             taken=getAns(arr,index-1,target-arr[index]);
+//         }
+//         return dp[index][target]=(notTaken+taken)%MOD;
+//     }
+//     int countPartitions(int n, int d, vector<int>& arr) {
+//         int totalSum=accumulate(arr.begin(),arr.end(),0);
+//         int reqdSum=totalSum-d;
+//         if(reqdSum%2!=0 || reqdSum<0){
+//             return 0;
+//         }
+//         reqdSum/=2;
+//         dp=vector<vector<int>>(n,vector<int>(reqdSum+1,-1));
+//         return getAns(arr,n-1,reqdSum);
+//     }
+// };
+
+//Tabulation Solution
+//     int countPartitions(int n, int d, vector<int>& arr) {
+//         int totalSum=accumulate(arr.begin(),arr.end(),0);
+//         int reqdSum=totalSum-d;
+//         if(reqdSum%2!=0 || reqdSum<0){
+//             return 0;
+//         }
+//         reqdSum/=2;
+//         dp=vector<vector<int>>(n,vector<int>(reqdSum+1,0));
+//         if(arr[0]==0){
+//             dp[0][0]=2;
+//         }
+//         else{
+//             dp[0][0]=1;
+//         }
+//         if(arr[0]!=0 && arr[0]<=reqdSum){
+//             dp[0][arr[0]]=1;
+//         }
+//         for(int index=1;index<n;index++){
+//             for(int target=0;target<=reqdSum;target++){
+//                 int notTaken=dp[index-1][target];
+//         int taken=0;
+//         if(arr[index]<=target){
+//             taken=dp[index-1][target-arr[index]];
+//         }
+//         dp[index][target]=(notTaken+taken)%MOD;
+//             }
+//         }
+//         return dp[n-1][reqdSum];
+//     }
+// };
+
+//Space Optimization
+//     int countPartitions(int n, int d, vector<int>& arr) {
+//         int totalSum=accumulate(arr.begin(),arr.end(),0);
+//         int reqdSum=totalSum-d;
+//         if(reqdSum%2!=0 || reqdSum<0){
+//             return 0;
+//         }
+//         reqdSum/=2;
+//         vector<int>prev(reqdSum+1,0),curr(reqdSum+1,0);
+//         if(arr[0]==0){
+//             prev[0]=2;
+//         }
+//         else{
+//             prev[0]=1;
+//         }
+//         if(arr[0]!=0 && arr[0]<=reqdSum){
+//             prev[arr[0]]=1;
+//         }
+//         for(int index=1;index<n;index++){
+//             for(int target=0;target<=reqdSum;target++){
+//                 int notTaken=prev[target];
+//         int taken=0;
+//         if(arr[index]<=target){
+//             taken=prev[target-arr[index]];
+//         }
+//             curr[target]=(notTaken+taken)%MOD;
+//             }
+//             prev=curr;
+//         }
+//         return prev[reqdSum];
+//     }
+// };
+
+//Coin Change
+//Memoization Solution
+// class Solution {
+// public:
+// vector<vector<int>>dp;
+//     int getAns(vector<int>&coins,int index,int target){
+//         if(target==0){
+//             return 0;
+//         }
+//         if(index<0){
+//             return 1e8;
+//         }
+//         if(dp[index][target]!=-1)return dp[index][target];
+//         //notTaken
+//         int notTaken=getAns(coins,index-1,target);
+//         int taken=INT_MAX;
+//         if(coins[index]<=target){
+//             taken=1+getAns(coins,index,target-coins[index]);
+//         }
+//         return dp[index][target]=min(notTaken,taken);
+//     }
+//     int coinChange(vector<int>& coins, int amount) {
+//         int n=coins.size();
+//         dp=vector<vector<int>>(n,vector<int>(amount+1,-1));
+//         int ans=getAns(coins,coins.size()-1,amount);
+//         if(ans==1e8){
+//             return -1;
+//         }
+//         return ans;
+//     }
+// };
+
+//Tabulation Solution
+//  int coinChange(vector<int>& coins, int amount) {
+//         int n=coins.size();
+//         dp=vector<vector<int>>(n,vector<int>(amount+1,1e8));
+//         for(int i=0;i<n;i++){
+//             dp[i][0]=0;
+//         }
+//         for(int i=1;i<=amount;i++){
+//           if (i % coins[0] == 0) {
+//             dp[0][i] = i / coins[0];
+//         }
+//         }
+//         for(int index=1;index<n;index++){
+//             for(int target=0;target<=amount;target++){
+//                 int notTaken=dp[index-1][target];
+//         int taken=1e8;;
+//         if(coins[index]<=target){
+//             taken=1+dp[index][target-coins[index]];
+//         }
+//              dp[index][target]=min(notTaken,taken);
+//             }
+//         }
+//         int ans=dp[n-1][amount];
+//         if(ans==1e8){
+//             return -1;
+//         }
+//         return ans;
+//     }
+
+//Space Optimisation
+//     int coinChange(vector<int>& coins, int amount) {
+//         int n=coins.size();
+// vector<int>prev(amount+1,1e8),curr(amount+1,1e8);
+//         prev[0]=0;
+//         for(int i=1;i<=amount;i++){
+//           if (i % coins[0] == 0) {
+//             prev[i] = i / coins[0];
+//         }
+//         }
+//         for(int index=1;index<n;index++){
+//             for(int target=0;target<=amount;target++){
+//                 int notTaken=prev[target];
+//         int taken=1e8;;
+//         if(coins[index]<=target){
+//             taken=1+curr[target-coins[index]];
+//         }
+//              curr[target]=min(notTaken,taken);
+//             }
+//             prev=curr;
+//         }
+//         int ans=prev[amount];
+//         if(ans==1e8){
+//             return -1;
+//         }
+//         return ans;
+//     }
+
+//Coin Change 2
+//Memoization Solution
+// class Solution {
+// public:
+//     vector<vector<int>>dp;
+//     int getAns(vector<int>&coins,int index,int target){
+//         if(target==0){
+//             return 1;
+//         }
+//         if(index<0){
+//             return 0;
+//         }
+//         if(dp[index][target]!=-1){
+//             return dp[index][target];
+//         }
+//         //not Taken
+//         int notTaken=getAns(coins,index-1,target);
+//         int taken=0;
+//         if(coins[index]<=target){
+//             taken=getAns(coins,index,target-coins[index]);
+//         }
+//         return dp[index][target]=notTaken+taken;
+//     }
+//     int change(int amount, vector<int>& coins) {
+//         int n=coins.size();
+//         dp=vector<vector<int>>(n,vector<int>(amount+1,-1));
+//         return getAns(coins,n-1,amount);
+//     }
+// };
+
+//Tabulation Solution
+// int change(int amount, vector<int>& coins) {
+//         int n=coins.size();
+//         dp=vector<vector<int>>(n,vector<int>(amount+1,0));
+//         for(int i=0;i<n;i++){
+//             dp[i][0]=1;
+//         }
+//         for(int i=1;i<=amount;i++){
+//             if(i%coins[0]==0){
+//                 dp[0][i]=1;
+//             }
+//         }
+//         for(int index=1;index<n;index++){
+//             for(int target=0;target<=amount;target++){
+//         //not Taken
+//         int notTaken=dp[index-1][target];
+//         int taken=0;
+//         if(coins[index]<=target){
+//             taken=dp[index][target-coins[index]];
+//         }
+//             dp[index][target]=notTaken+taken;
+//             }
+//         }
+// return dp[n-1][amount];
+//     }
+
+//Space Optimisation Solution
+//     int change(int amount, vector<int>& coins) {
+//         int n=coins.size();
+//         vector<int>prev(amount+1,0),curr(amount+1,0);
+//      prev[0]=1;
+//         for(int i=1;i<=amount;i++){
+//             if(i%coins[0]==0){
+//                 prev[i]=1;
+//             }
+//         }
+//         for(int index=1;index<n;index++){
+//             for(int target=0;target<=amount;target++){
+//         //not Taken
+//         int notTaken=prev[target];
+//         int taken=0;
+//         if(coins[index]<=target){
+//             taken=curr[target-coins[index]];
+//         }
+//             curr[target]=notTaken+taken;
+//             }
+//             prev=curr;
+//         }
+// return prev[amount];
+//     }
+
+
+
+
