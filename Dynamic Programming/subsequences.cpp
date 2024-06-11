8d7a600e66cc6e2af3f43ee97d98e2420cf8ed30
tusharsingh1702 <tusharsingh1702@gmail.com>
2024-06-11 23:45:13 +0530

    Update subsequences.cpp

--- Dynamic Programming/subsequences.cpp
+++ Dynamic Programming/subsequences.cpp
@@ -690,5 +690,58 @@
 //     }
 // };
 
+//Shortest common supersequence
+// class Solution {
+// public:
+//     string shortestCommonSupersequence(string str1, string str2) {
+//         int n1=str1.length(),n2=str2.length();
+//         vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+//         string str="";
+//         for(int i=1;i<=n1;i++){
+//             for(int j=1;j<=n2;j++){
+//                 if(str1[i-1]==str2[j-1]){
+//                     dp[i][j]=1+dp[i-1][j-1];
+//                 }
+//                 else{
+//                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+//                 }
+//             }
+//         }
+//         int i=n1,j=n2;
+//         while(i>0 && j>0){
+//             if(str1[i-1]==str2[j-1]){
+//                 str.push_back(str1[i-1]);
+//                 i--;
+//                 j--;
+//             }
+//             else{
+//                 if(dp[i-1][j]>dp[i][j-1]){
+//                    str.push_back(str1[i-1]);
+//                    i--;
+//                 }
+//                 else{
+//                    str.push_back(str2[j-1]);
+//                    j--;
+//                 }
+//             }
+//         }
+
+//             while(j>0){
+//                 str.push_back(str2[j-1]);
+//                 j--;
+//             }
+
+
+//             while(i>0){
+//                 str.push_back(str1[i-1]);
+//                 i--;
+//             }
+
+//         reverse(str.begin(),str.end());
+//         return str;
+
+//     }
+// };
+
 
 
