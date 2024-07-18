b51ac5c3ab48d291f04b5a97a3e35a02498683ed
tusharsingh1702 <tusharsingh1702@gmail.com>
2024-07-19 01:38:38 +0530

    Update prefixinfixpostfix.cpp

--- Stack/Queues/prefixinfixpostfix.cpp
+++ Stack/Queues/prefixinfixpostfix.cpp
@@ -51,3 +51,95 @@
 //         return ans;
 //     }
 // };
+
+// Postfix to infix conversion
+// class Solution {
+//   public:
+//     string postToInfix(string exp) {
+//            stack<string>stk;
+//         int i=0,n=exp.length();
+//         while(i<n){
+//             if( isalnum(exp[i])){
+//                 stk.push(string(1,exp[i]));
+//             }
+//             else{
+//                 string top1=stk.top();
+//                 stk.pop();
+//                 string top2=stk.top();
+//                 stk.pop();
+//                 stk.push("("+top2+exp[i]+top1+")");
+//             }
+//             i++;
+//         }
+//         return stk.top();
+//     }
+// };
+
+//Prefix to Infix conversion
+// class Solution {
+//   public:
+//     string preToInfix(string pre_exp) {
+//         int i=pre_exp.length()-1;
+//         stack<string>stk;
+//         while(i>=0){
+//             if(isalpha(pre_exp[i]) || isdigit(pre_exp[i])){
+//                 stk.push(string(1,pre_exp[i]));
+//             }
+//             else{
+//                 string top1=stk.top();
+//                 stk.pop();
+//                 string top2=stk.top();
+//                 stk.pop();
+//                 stk.push("("+top1+pre_exp[i]+top2+")");
+//             }
+//             i--;
+//         }
+//         return stk.top();
+//     }
+// };
+
+//Postfix to prefix
+// class Solution {
+//   public:
+//     string postToPre(string post_exp) {
+//         stack<string>stk;
+//         int i=0,n=post_exp.length();
+//         while(i<n){
+//             if(isalnum(post_exp[i])){
+//                 stk.push(string(1,post_exp[i]));
+//             }
+//             else{
+//                 string top1=stk.top();
+//                 stk.pop();
+//                 string top2=stk.top();
+//                 stk.pop();
+//                 stk.push(post_exp[i]+top2+top1);
+//             }
+//             i++;
+//         }
+//         return stk.top();
+//     }
+// };
+
+// Prefix to postfix
+// class Solution {
+//   public:
+//     string preToPost(string pre_exp) {
+//         stack<string>stk;
+//         int i=pre_exp.length()-1;
+//         while(i>=0){
+//             if(isalnum(pre_exp[i])){
+//                 stk.push(string(1,pre_exp[i]));
+//             }
+//             else{
+//                 string top1=stk.top();
+//                 stk.pop();
+//                 string top2=stk.top();
+//                 stk.pop();
+//                 stk.push(top1+top2+pre_exp[i]);
+//             }
+//             i--;
+//         }
+//         return stk.top();
+//     }
+// };
